0a5966d999542ddf9713ccc61daa4b3f23bdcd3e
FlufficornTokenMaker <flufficorn@sdf.org>
2023-08-06 07:44:01 +0000

    Flufficorn Token Website

---
+++ js/scripts.js
@@ -0,0 +1,82 @@
+/*!
+* Start Bootstrap - Freelancer v7.0.7 (https://startbootstrap.com/theme/freelancer)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-freelancer/blob/master/LICENSE)
+*/
+//
+// Scripts
+//
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    // Shrink the navbar
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            rootMargin: '0px 0px -40%',
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
+
+const addToMetamask = async () => {
+    try {
+        await window.ethereum.request({
+            "method": "wallet_switchEthereumChain",
+            "params": [
+                {
+                    "chainId": "0x2105"
+                }
+            ]
+        });
+        await ethereum.request({
+            method: 'wallet_watchAsset',
+            params: {
+                type: 'ERC20',
+                options: {
+                    address: '0x891B7fBf3429441cc715e09aeD43F4Cd53c50A73',
+                    symbol: 'FCORN',
+                    decimals: 18,
+                    image: 'https://flufficorntoken.github.io/assets/img/logo.png'
+                }
+            }
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+document.getElementById('addToMetamask').addEventListener('click', addToMetamask);
